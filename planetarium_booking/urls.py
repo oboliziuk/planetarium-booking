e2d130dc6d911ff518824e26c2a39c5d4c661a72
Oksana Boliziuk <oksanabolizyk23@gmail.com>
2025-03-12 14:06:58 +0200

    Set up URLs and link planetarium app in planetarium_booking/urls.py

--- planetarium_booking/urls.py
+++ planetarium_booking/urls.py
@@ -24,4 +24,20 @@ from drf_spectacular.views import (
     SpectacularRedocView,
 )
 
-git
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/planetarium/", include("planetarium.urls", namespace="planetarium")),
+    path("api/user/", include("user.urls", namespace="user")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/doc/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
+    path("__debug__/", include("debug_toolbar.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
