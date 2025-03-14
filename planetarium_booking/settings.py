9146555acd496f87a64bf362497ddea58d8f26a6
Oksana Boliziuk <oksanabolizyk23@gmail.com>
2025-03-14 09:50:25 +0200

    Move SECRET_KEY to .env

--- planetarium_booking/settings.py
+++ planetarium_booking/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from datetime import timedelta
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-@z#aww-(a-)qtv3_&y#yacn9udc)u@zurn6o+yw3ut1&#4#+_t"
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
