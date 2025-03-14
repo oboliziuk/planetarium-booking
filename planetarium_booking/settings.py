6031ee652ae6e1ec3c243b6552f58fc1954d8fea
Oksana Boliziuk <oksanabolizyk23@gmail.com>
2025-03-14 09:58:26 +0200

    Move SECRET_KEY to .env

--- planetarium_booking/settings.py
+++ planetarium_booking/settings.py
@@ -21,9 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 load_dotenv()
 
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
